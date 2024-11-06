4f98d68057c7d5739c97e6ff6b6fd97d6b4ea938
Istiqoomah Khoirunnisaa <istiqomahkhoerunnisa@gmail.com>
2024-11-06 11:41:46 +0700

    mengedit ratings 1

---
+++ app/Models/Rating.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Rating extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    protected $fillable = [
+        'comment',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User');
+    }
+
+    public function tipekamar()
+    {
+        return $this->belongsTo('App\Models\TipeKamar');
+    }
+
+    public function ratings ()
+    {
+        return $this->hasMany('App\Models\Rating');
+    }
+}
