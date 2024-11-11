cb121b2dc68bf8818eed481cfdd126e4cca18fd9
Syafira Nur Azizah <syafiranurazizah1710@gmail.com>
2024-11-11 21:48:38 +0700

    tampilan form booking (belum selesai)

--- resources/views/hotel/detail-hotel.blade.php
+++ resources/views/hotel/detail-hotel.blade.php
@@ -1,150 +1,8 @@
 @extends('layouts.app')
 
-{{-- @push('styles')
-<style>
-    .room-details-item img {
-        width: 100%;
-        height: auto;
-        border-radius: 12px;
-    }
-
-    .rd-text {
-        margin-top: 20px;
-    }
-
-    .rd-title {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-    }
-
-    .rd-title h3 {
-        font-size: 24px;
-        margin-bottom: 10px;
-    }
-
-    .rd-title .rdt-right {
-        display: flex;
-        align-items: center;
-    }
-
-    .rdt-right .rating {
-        margin-right: 20px;
-    }
-
-    .rdt-right a {
-        background-color: #ff9c00;
-        padding: 10px 20px;
-        color: white;
-        border-radius: 5px;
-        text-decoration: none;
-    }
-
-    .rd-text h2 {
-        font-size: 28px;
-        margin-top: 15px;
-        margin-bottom: 25px;
-    }
-
-    .rd-text table {
-        width: 100%;
-        margin-bottom: 20px;
-    }
-
-    .rd-text table td {
-        padding: 10px 0;
-    }
-
-    .rd-text table .r-o {
-        font-weight: bold;
-    }
-
-    .rd-reviews {
-        margin-top: 40px;
-    }
-
-    .review-item {
-        display: flex;
-        margin-bottom: 20px;
-    }
-
-    .ri-pic img {
-        width: 60px;
-        border-radius: 50%;
-        margin-right: 20px;
-    }
-
-    .ri-text {
-        flex-grow: 1;
-    }
-
-    .ri-text .rating {
-        margin-bottom: 5px;
-    }
-
-    .room-booking {
-        background-color: #f3f4f5;
-        padding: 30px;
-        border-radius: 10px;
-        margin-top: 30px;
-    }
-
-    .room-booking h3 {
-        margin-bottom: 20px;
-    }
-
-    .check-date {
-        margin-bottom: 15px;
-    }
-
-    .check-date input {
-        width: calc(100% - 30px);
-        padding: 10px;
-        border: 1px solid #e1e1e1;
-        border-radius: 5px;
-    }
-
-    .check-date i {
-        position: relative;
-        top: -30px;
-        right: -10px;
-        color: #999;
-    }
-
-    .select-option {
-        margin-bottom: 15px;
-    }
-
-    .select-option select {
-        width: 100%;
-        padding: 10px;
-        border: 1px solid #e1e1e1;
-        border-radius: 5px;
-    }
-
-    .room-booking button {
-        width: 100%;
-        background-color: #ff9c00;
-        border: none;
-        padding: 10px;
-        color: white;
-        border-radius: 5px;
-        font-size: 16px;
-    } --}}
-
-    {{-- /* Responsive for mobile */
-    @media (max-width: 768px) {
-        .rd-title {
-            flex-direction: column;
-            align-items: flex-start;
-        }
-
-        .room-booking {
-            margin-top: 20px;
-        }
-    }
-</style>
-@endpush --}}
+@push('styles')
+    <link rel="stylesheet" href="{{ asset('css/detail-hotel.css') }}">
+@endpush
 
 @section('content')
 <br><br><br><br>
@@ -262,11 +120,14 @@
                         </form>
                     </div>
                 </div>
+
+
+
                 <div class="col-lg-4">
                     @foreach($hotels as $hotel)
                         <div class="room-booking">
                             {{-- <h3>Your Reservation at {{ $hotel->nama_cabang }} - {{ $room->nama_tipe }}</h3> --}}
-                            <h3>Your Reservation</h3>
+                            <h3>Reservasi</h3>
                             <form action="{{ route('booking.hotel.store', ['location' => strtolower($hotel->nama_cabang), 'nama_tipe' => $room->nama_tipe]) }}" method="POST">
                                 @csrf
                 
@@ -359,36 +220,6 @@
 @push('scripts')
 
 <script>
-    // $(function() {
-    //     $("#date-in").datepicker({
-    //         dateFormat: 'yy-mm-dd',
-    //         onSelect: function() {
-    //             updateCheckout();
-    //         }
-    //     });
-    //     $("#date-out").datepicker({
-    //         dateFormat: 'yy-mm-dd'
-    //     });
-    // });
-
-    // function updateCheckout() {
-    //     const checkinInput = document.getElementById('date-in');
-    //     const checkoutInput = document.getElementById('date-out');
-
-    //     const checkinDate = new Date(checkinInput.value);
-    //     if (!isNaN(checkinDate.getTime())) {
-    //         checkinDate.setDate(checkinDate.getDate() + 1);
-    //         const year = checkinDate.getFullYear();
-    //         const month = String(checkinDate.getMonth() + 1).padStart(2, '0');
-    //         const day = String(checkinDate.getDate()).padStart(2, '0');
-    //         checkoutInput.value = `${year}-${month}-${day}`;
-    //     } else {
-    //         checkoutInput.value = '';
-    //     }
-    // };
-
-
-
     document.addEventListener('DOMContentLoaded', function () {
         const forms = document.querySelectorAll('.room-booking form');
 
